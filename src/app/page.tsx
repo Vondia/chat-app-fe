5cad9ea4e75a5a73e242e11ee43e20ebff912d52
vondia <pimvdbosch@gmail.com>
2024-09-16 15:18:28 +0200

    added the chat with styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client"
+import { Chat } from '@/components/chat';
 import { css } from '../../styled-system/css';
+import { useState } from 'react';
 
 export default function Home() {
+    const [currentUser, setCurrentUser] = useState(null);
   return (
-    <div className={css({ fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼!</div>
+    <div className={css({ display: "flex", width: 'full', height: '100vh', alignItems: 'center', justifyContent: 'center' })}>
+      <Chat currentUser={currentUser} onLogout={() => setCurrentUser(null)} />
+  </div>
   )
 }
---
+++ src/components/chat.tsx
@@ -0,0 +1,104 @@
+"use client";
+import { useState, useEffect, KeyboardEvent, ChangeEvent } from "react";
+import { io, Socket } from "socket.io-client";
+import * as styles from "./chat.styles"; // Importing all styles under the `styles` namespace
+
+interface Message {
+  id: number;
+  body: string;
+  author: string;
+}
+
+interface ChatProps {
+  currentUser: string;
+  onLogout: () => void;
+}
+
+const SystemMessage: Message = {
+  id: 1,
+  body: "Welcome to the Nest Chat app",
+  author: "Bot",
+};
+
+const socket: Socket = io('http://localhost:4000', { autoConnect: false });
+
+export function Chat({ currentUser, onLogout }: ChatProps) {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [messages, setMessages] = useState<Message[]>([SystemMessage]);
+
+  useEffect(() => {
+    socket.connect();
+
+    socket.on("connect", () => {
+      console.log("Socket connected");
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected");
+    });
+
+    socket.on("chat", (newMessage: Message) => {
+      console.log("New message added", newMessage);
+      setMessages((previousMessages) => [...previousMessages, newMessage]);
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("chat");
+    };
+  }, []);
+
+  const handleSendMessage = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key !== "Enter" || inputValue.trim().length === 0) return;
+
+    socket.emit("chat", { author: currentUser, body: inputValue.trim() });
+    setInputValue("");
+  };
+
+  const handleLogout = () => {
+    socket.disconnect();
+    onLogout();
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div className={styles.chat()}>
+      <div className={styles.chatHeader()}>
+        <span>Nest Chat App</span>
+        <button className={styles.button()} onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
+      <div className={styles.chatMessageList()}>
+        {messages.map((message, idx) => (
+          <div
+            key={idx}
+            className={`${styles.chatMessage()} ${
+              currentUser === message.author ? styles.outgoing() : ""
+            }`}
+          >
+            <div className={styles.chatMessageWrapper()}>
+              <span className={styles.chatMessageAuthor()}>{message.author}</span>
+              <div className={styles.chatMessageBubble()}>
+                <span className={styles.chatMessageBody()}>{message.body}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={styles.chatComposer()}>
+        <input
+          className={styles.chatComposerInput()}
+          placeholder="Type message here"
+          value={inputValue}
+          onChange={handleChange}
+          onKeyDown={handleSendMessage}
+        />
+      </div>
+    </div>
+  );
+}
